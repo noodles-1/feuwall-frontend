390ef697814064acb95379a766a59ed0cc9d9355
noodelz <adrianeroa081202@gmail.com>
2023-09-28 10:35:00 +0800

    changed footer, website name and create submission: margin bottom

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer">
                 <div className="footer-elements">
                     <div className="footer-logo">
-                        <h1 className="text-left text-5xl font-semibold"> [feu-wall] </h1>
+                        <h1 className="text-left text-4xl font-semibold"> [dattebayo-wall] </h1>
                     </div>
                     <div className="footer-middle">
                         <NavLink to="home"> All confessions </NavLink>
